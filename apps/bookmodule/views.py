29a8e56956bd02f98baabf4598a508d833bd3dcd
Reema <reema64n@gmail.com>
2026-09-22 01:53:46 +0300

    Complete Lab 3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    name = request.GET.get("name")
+    return render(request, "index.html" , {"name": name})
+
+def index2(request, val1):
+    return HttpResponse(f"Value is {val1}")
+
+def viewbook(request):
+    books = [
+        "Python Programming",
+        "Django for Beginners",
+        "Web Technologies",
+        "Artificial Intelligence"
+    ]
+    return render(request, "show.html", {"books": books})
